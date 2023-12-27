dd02c7adbe2d30c89fdc1795a23617fd51b1dae7
Md Yeasir Arafat <frahim5@gmail.com>
2023-12-27 12:13:44 +0600

    update

--- components/header.js
+++ components/header.js
@@ -15,13 +15,13 @@ export default function header() {
                     </div>
                     <div className="grid-item text-xl-center d-flex align-items-center gap-4">
                         <p className="mb-0 ff-inter text-dark3 fs-14 fw-medium">{"Propose a user to Join Global Life Community"}</p>
-                        <Link href="#" className="ff-inter bg-green proposeBtn d-flex align-items-center justify-content-center text-white text-decoration-none px-2 py-1 fs-12">propose <span><svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <Link href="/" className="ff-inter bg-green proposeBtn d-flex align-items-center justify-content-center text-white text-decoration-none px-2 py-1 fs-12">propose <span><svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M11.2295 5.64551L5.69531 0.842285C5.65283 0.805664 5.59863 0.785156 5.5415 0.785156H4.24512C4.13672 0.785156 4.08691 0.919922 4.16895 0.990234L9.29883 5.44336H0.726562C0.662109 5.44336 0.609375 5.49609 0.609375 5.56055V6.43945C0.609375 6.50391 0.662109 6.55664 0.726562 6.55664H9.29736L4.16748 11.0098C4.08545 11.0815 4.13525 11.2148 4.24365 11.2148H5.58398C5.61182 11.2148 5.63965 11.2046 5.66016 11.1855L11.2295 6.35449C11.2802 6.3104 11.3209 6.25593 11.3487 6.19478C11.3766 6.13362 11.391 6.0672 11.391 6C11.391 5.9328 11.3766 5.86638 11.3487 5.80522C11.3209 5.74407 11.2802 5.6896 11.2295 5.64551Z"
                                 fill="white" /> </svg></span></Link>
                     </div>
                     <div className="grid-item d-flex align-items-center justify-content-center justify-content-sm-end gap-3">
-                        <Link href="#" className="fs-12 fw-semiBold text-dark1 ff-inter text-uppercase text-decoration-none">Login</Link>
-                        <Link href="#" className="fs-12 fw-semiBold text-dark1 ff-inter text-uppercase text-decoration-none">Apply</Link>
+                        <Link href="/" className="fs-12 fw-semiBold text-dark1 ff-inter text-uppercase text-decoration-none">Login</Link>
+                        <Link href="/" className="fs-12 fw-semiBold text-dark1 ff-inter text-uppercase text-decoration-none">Apply</Link>
                     </div>
                 </div>
             </div>
