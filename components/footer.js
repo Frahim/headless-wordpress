78d740b1d63d84f55f5df4c9a3393c5071279f62
Md Yeasir Arafat <frahim5@gmail.com>
2023-12-27 12:08:29 +0600

    update

--- components/footer.js
+++ components/footer.js
@@ -16,42 +16,42 @@ export default function footer() {
                     <div className="row footer-grid">
                         <div className="col-6 col-xl-3 col-md-3 grid-item">
                             <div className="footer-widget mb-4 wow fadeInDown">
-                                <h6 className="text-white mb-4 fs-24 fw-semiBold">Global Life</h6>
+                                <h6 className="text-white mb-4 fs-24 fw-semiBold">{"Global Life"}</h6>
                                 <ul className="list-unstyled mb-0">
-                                    <li className="mb-2"><Link href="#">About</Link> </li>
-                                    <li className="mb-2"><Link href="#">career</Link> </li>
-                                    <li className="mb-2"><Link href="#">Contact</Link> </li>
-                                    <li className="mb-2"><Link href="#">Terms & Conditions</Link> </li>
-                                    <li className="mb-2"><Link href="#">Privacy Policy</Link> </li>
+                                    <li className="mb-2"><Link href="#">{"About"}</Link> </li>
+                                    <li className="mb-2"><Link href="#">{"career"}</Link> </li>
+                                    <li className="mb-2"><Link href="#">{"Contact"}</Link> </li>
+                                    <li className="mb-2"><Link href="#">{"Terms & Conditions"}</Link> </li>
+                                    <li className="mb-2"><Link href="#">{"Privacy Policy"}</Link> </li>
                                 </ul>
                             </div>
                         </div>
                         <div className="col-6 col-xl-3 col-md-4 grid-item">
                             <div className="footer-widget mb-4 wow fadeInDown">
-                                <h6 className="text-white mb-4 fs-24 fw-semiBold">Explore</h6>
+                                <h6 className="text-white mb-4 fs-24 fw-semiBold">{"Explore"}</h6>
                                 <ul className="list-unstyled mb-0">
-                                    <li className="mb-2"><Link href="#">What's happening?</Link> </li>
-                                    <li className="mb-2"><Link href="#">Recommendations</Link> </li>
-                                    <li className="mb-2"><Link href="#">Trading Post</Link> </li>
-                                    <li className="mb-2"><Link href="#">Challenges</Link> </li>
+                                    <li className="mb-2"><Link href="#">{"What's happening?"}</Link> </li>
+                                    <li className="mb-2"><Link href="#">{"Recommendations"}</Link> </li>
+                                    <li className="mb-2"><Link href="#">{"Trading Post"}</Link> </li>
+                                    <li className="mb-2"><Link href="#">{"Challenges"}</Link> </li>
                                 </ul>
                             </div>
                         </div>
                         <div className="col-6 col-xl-3 col-md-4 grid-item">
                             <div className="footer-widget mb-4 wow fadeInDown">
-                                <h6 className="text-white mb-4 fs-24 fw-semiBold">Stay Connected</h6>
+                                <h6 className="text-white mb-4 fs-24 fw-semiBold">{"Stay Connected"}</h6>
                                 <ul className="list-unstyled mb-0">
-                                    <li className="mb-2"><span><Image src={social1} width={25} height={15} className="img-fluid pe-2" alt="social" /></span><Link href="#"> Facebook </Link> </li>
-                                    <li className="mb-2"><span><Image src={social2} width={25} height={15}  className="img-fluid pe-2" alt="social" /></span><Link href="#"> Twitter </Link> </li>
-                                    <li className="mb-2"><span><Image src={social3} width={25} height={15} className="img-fluid pe-2" alt="social" /></span><Link href="#"> Instagram </Link> </li>
-                                    <li className="mb-2"><span><Image src={social4} width={25} height={15}  className="img-fluid pe-2" alt="social" /></span><Link href="#"> LinkedIn </Link> </li>
+                                    <li className="mb-2"><span><Image src={social1} width={25} height={15} className="img-fluid pe-2" alt="social" /></span><Link href="#">{"Facebook"}  </Link> </li>
+                                    <li className="mb-2"><span><Image src={social2} width={25} height={15}  className="img-fluid pe-2" alt="social" /></span><Link href="#">{"Twitter"}  </Link> </li>
+                                    <li className="mb-2"><span><Image src={social3} width={25} height={15} className="img-fluid pe-2" alt="social" /></span><Link href="#">{"Instagram"}  </Link> </li>
+                                    <li className="mb-2"><span><Image src={social4} width={25} height={15}  className="img-fluid pe-2" alt="social" /></span><Link href="#">{"LinkedIn"}  </Link> </li>
 
                                 </ul>
                             </div>
                         </div>
                         <div className="col-6 col-xl-3 col-md-4 grid-item">
                             <div className="footer-widget mb-4 wow fadeInDown">
-                                <h6 className="text-white mb-5 fs-24 fw-semiBold">Newsletter Signup</h6>
+                                <h6 className="text-white mb-5 fs-24 fw-semiBold">{"Newsletter Signup"}</h6>
                                 <ul className="list-unstyled mb-0 NewsletterWrapper">
                                     <li className="mb-1">
                                         <input type="email" placeholder="Email" className="emailControl ff-inter text-white" />
@@ -66,7 +66,7 @@ export default function footer() {
             <div className="footer-bottom py-4">
                 <div className="container">
                     <p className="mb-0 ff-14 fw-medium text-dark4 text-center  mb-4 mb-xl-0 text-center ff-inter w-100 d-flex justify-content-center">
-                        <small>© 2022 Global Life. All rights reserved.</small>
+                        <small>{"© 2022 Global Life. All rights reserved."}</small>
                     </p>
                 </div>
             </div>
--- components/header.js
+++ components/header.js
@@ -14,7 +14,7 @@ export default function header() {
                         </div>
                     </div>
                     <div className="grid-item text-xl-center d-flex align-items-center gap-4">
-                        <p className="mb-0 ff-inter text-dark3 fs-14 fw-medium">Propose a user to Join Global Life Community</p>
+                        <p className="mb-0 ff-inter text-dark3 fs-14 fw-medium">{"Propose a user to Join Global Life Community"}</p>
                         <Link href="#" className="ff-inter bg-green proposeBtn d-flex align-items-center justify-content-center text-white text-decoration-none px-2 py-1 fs-12">propose <span><svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M11.2295 5.64551L5.69531 0.842285C5.65283 0.805664 5.59863 0.785156 5.5415 0.785156H4.24512C4.13672 0.785156 4.08691 0.919922 4.16895 0.990234L9.29883 5.44336H0.726562C0.662109 5.44336 0.609375 5.49609 0.609375 5.56055V6.43945C0.609375 6.50391 0.662109 6.55664 0.726562 6.55664H9.29736L4.16748 11.0098C4.08545 11.0815 4.13525 11.2148 4.24365 11.2148H5.58398C5.61182 11.2148 5.63965 11.2046 5.66016 11.1855L11.2295 6.35449C11.2802 6.3104 11.3209 6.25593 11.3487 6.19478C11.3766 6.13362 11.391 6.0672 11.391 6C11.391 5.9328 11.3766 5.86638 11.3487 5.80522C11.3209 5.74407 11.2802 5.6896 11.2295 5.64551Z"
                                 fill="white" /> </svg></span></Link>
@@ -37,13 +37,13 @@ export default function header() {
                                 d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
                         </svg>
                     </button>
-                    <Link href="index.html" className="brand-logo logo is-active-mobile d-md-none">
+                    <Link href="/" className="brand-logo logo is-active-mobile d-md-none">
                         <Image src={logo}  alt="img" className="img-fluid" width={119} height={66}/>
                     </Link>
                     <div className="navbar px-3 py-4 py-md-0 px-md-0">
                         <div className="navbar-header d-flex d-md-none align-items-center justify-content-between mb-4">
-                            <Link href="index.html" className="brand-logo logo is-active-mobile d-md-none">
-                                <Image src="/image/logo.svg" alt="img" className="img-fluid" width={119} height={66}/>
+                            <Link href="/" className="brand-logo logo is-active-mobile d-md-none">
+                                <Image src={logo} alt="img" className="img-fluid" width={119} height={66}/>
                             </Link>
                             <button
                                 className="btn btn-menu-toggle btn-default d-flex d-md-none align-items-center justify-content-center">
@@ -57,11 +57,11 @@ export default function header() {
                         <ul id="menu" className="d-md-flex align-items-center justify-content-between m-0 p-0 gap-5 w-100">
                             <li><Link href="/" className=" ff-inter navLink">What's happening?</Link></li>
                             <li>
-                                <Link href="#" className="menuLink ff-inter navLink">Recommendations</Link>
+                                <Link href="/" className="menuLink ff-inter navLink">Recommendations</Link>
                             </li>
                             <li className="d-none d-md-block">
-                                <Link href="index.html" className="logo">
-                                    <Image src="/image/logo.svg" alt="img" className="img-fluid" width={119} height={66} />
+                                <Link href="/" className="logo">
+                                    <Image src={logo} alt="img" className="img-fluid" width={119} height={66} />
                                 </Link>
                             </li>
                             <li><Link href="/" className="menuLink ff-inter navLink">Trading Post</Link></li>
