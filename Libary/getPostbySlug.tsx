3fb96de7773fb2cf85dac618f3dbdf9fb00849b5
Md Yeasir Arafat <frahim5@gmail.com>
2024-06-24 13:16:02 +0600

    Update getPostbySlug.tsx

--- Libary/getPostbySlug.tsx
+++ Libary/getPostbySlug.tsx
@@ -1,5 +1,5 @@
 export default async function getPostySlug(slug: string) {
-    const res = await fetch(`https://uiexpertz.com/globallife-wp/wp-json/wp/v2/posts?_embed&slug=${slug}`);
+    const res = await fetch(`https://gblheadlesswp.uiexpertz.com/wp-json/wp/v2/posts?_embed&slug=${slug}`);
     if (!res.ok) {
       throw new Error('Failed to load API');
     }
